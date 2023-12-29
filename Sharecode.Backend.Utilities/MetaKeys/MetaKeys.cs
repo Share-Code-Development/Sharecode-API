001f8b86933754377115024d50dbad09e7aaa877
Alen Alex <alengeoalex@gmail.com>
2023-12-29 23:03:49 +0530

    Update casing of meta keys strings
    The casing of two keys, 'LimitComments' and 'RecentSnippets', in MetaKeys class has been changed. This amendment ensures these keys match the expected format in their respective invoking methods.

--- Sharecode.Backend.Utilities/MetaKeys/MetaKeys.cs
+++ Sharecode.Backend.Utilities/MetaKeys/MetaKeys.cs
@@ -4,11 +4,11 @@ public static class MetaKeys
 {
     public static class SnippetKeys
     {
-        public static MetaKey LimitComments => new("LimitComments", typeof(bool));
+        public static MetaKey LimitComments => new("limitComments", typeof(bool));
     }
 
     public static class UserKeys
     {
-        public static MetaKey RecentlyVisitedSnippets => new("RecentSnippets", typeof(List<Guid>));
+        public static MetaKey RecentlyVisitedSnippets => new("recentSnippets", typeof(List<Guid>));
     }
 }
